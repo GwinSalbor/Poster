ebe3711f32e2c7d3e854370e9ebcf3445e90e7c9
roman412 <romanio412@gmail.com>
2017-09-06 18:55:35 +0300

    Восстановлены недостающие файлы

---
+++ Poster/Models/Release/Artist.cs
@@ -0,0 +1,8 @@
+﻿namespace Poster
+{
+    public class Artist
+    {
+        public int Id { get; internal set; }
+        public string Name { get; internal set; }
+    }
+}
---
+++ Poster/Models/Release/Label.cs
@@ -0,0 +1,9 @@
+﻿namespace Poster
+{
+    public class Label
+    {
+        public int Id { get; internal set; }
+        public string Name { get; internal set; }
+        public string CatNo { get; internal set; }
+    }
+}
